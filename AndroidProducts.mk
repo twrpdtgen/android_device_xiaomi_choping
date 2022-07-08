5b777bf9ad236c421ddcf88599f7ffe5aafb8cfc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-08 13:12:39 +0200

    choping: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 21061110AG Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_choping.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_choping-user \
+    omni_choping-userdebug \
+    omni_choping-eng
